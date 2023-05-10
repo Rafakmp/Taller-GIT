b8d665c9e62c5657d557e580289e9d011359a494
Rafakmp <mpr0033079@est.univalle.edu>
2023-05-10 14:19:33 -0400

    mi 1er hola mundo

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //a. Escribe un programa en C# que lea N números. Debes imprimir la cantidad de impares negativos y la cantidad de pares positivos. 
+            int N, CI, CP;
+            CI = 9;
+                CP = 7;
+            N = CI + CP;
+        }
+    }
+}
